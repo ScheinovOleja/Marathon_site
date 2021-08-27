03f42b09a9f38c8d75c1103cd3bb5a36bd1c5548
oleg <oleg20064@gmail.com>
2021-08-27 11:39:13 +0300

    Дебаг на сервере.

--- marathon_bot/scenarios/handler_register.py
+++ marathon_bot/scenarios/handler_register.py
@@ -146,7 +146,7 @@ async def register_marathon(query: types.CallbackQuery, state: FSMContext):
             first_name='.',
             last_name='.',
             scopes=0,
-            marathon=marathon,
+            marathon_id=marathon.id,
             is_pay=False,
         )
         commit()
