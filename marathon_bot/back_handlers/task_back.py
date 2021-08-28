379fba4044ce5375c4db9a3e85426c462af5894f
oleg <oleg20064@gmail.com>
2021-08-28 09:31:21 +0300

    исправил пока заданий

--- marathon_bot/back_handlers/task_back.py
+++ marathon_bot/back_handlers/task_back.py
@@ -7,12 +7,16 @@ from marathon_bot.states.all_states_menu import TaskMenu
 
 async def back_to_category(query: CallbackQuery, state: FSMContext):
     await TaskMenu.previous()
+    data = await state.get_data()
+    await state.set_data({'marathon_id': data['marathon_id'])
     return await send_category_tasks(query, state)
 
 
 async def back_to_list_tasks(query: CallbackQuery, state: FSMContext):
     await TaskMenu.previous()
     await TaskMenu.previous()
+    data = await state.get_data()
+    await state.set_data({'marathon_id': data['marathon_id'], 'callback': data['callback']})
     return await send_tasks(query, state)
 
 
